ff8a6ef3638048ec345f8a2d108ed3e5cc76559f
Shivam Singh <shivamsi687@gmail.com>
2022-08-19 23:11:09 +0530

    Updating Some Enity

---
+++ dist/config/env/production/database.js
@@ -0,0 +1,33 @@
+"use strict";
+// module.exports = ({ env }) => ({
+//   connection: {
+//     client: 'postgres',
+//     connection: {
+//       host: env('DATABASE_HOST', 'localhost'),
+//       port: env.int('DATABASE_PORT', 5432),
+//       database: env('DATABASE_NAME', 'bank'),
+//       user: env('DATABASE_USERNAME', 'postgres'),
+//       password: env('DATABASE_PASSWORD', '0000'),
+//       schema: env('DATABASE_SCHEMA', 'public'), // Not required
+//       ssl: {
+//         // rejectUnauthorized: env.bool('DATABASE_SSL_SELF', false),
+//         ca: env('DATABASE_CA'),
+//       },
+//     },
+//     debug: false,
+//   },
+// });
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.default = ({ env }) => ({
+    connection: {
+        client: 'mysql',
+        connection: {
+            host: env('DATABASE_HOST', '127.0.0.1'),
+            port: env.int('DATABASE_PORT', 3306),
+            database: env('DATABASE_NAME', 'iterlef_blog'),
+            user: env('DATABASE_USERNAME', 'root'),
+            password: env('DATABASE_PASSWORD', ''),
+            ssl: env.bool('DATABASE_SSL', false),
+        },
+    },
+});
---
+++ dist/src/api/lead/services/lead.js
@@ -0,0 +1,7 @@
+"use strict";
+/**
+ * lead service.
+ */
+Object.defineProperty(exports, "__esModule", { value: true });
+const strapi_1 = require("@strapi/strapi");
+exports.default = strapi_1.factories.createCoreService('api::lead.lead');
